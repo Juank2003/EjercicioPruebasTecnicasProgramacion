1592997074b785ddabb80afd33023ca56a03bc10
jkort <jortealv@myuax.com>
2023-03-25 12:58:36 +0100

    AñadirExcepcionesAlMetodo

--- src/main/java/pruebas/ClaseCadena.java
+++ src/main/java/pruebas/ClaseCadena.java
@@ -3,18 +3,28 @@ package pruebas;
 public class ClaseCadena {
 
     public static String devuelveIniciales(String cadena){
-        String[] palabras = cadena.split(" ");
+        String inicial = "";
+        try {
+            String[] palabras = cadena.split(" ");
 
-        if(palabras.length < 1){
-            return "";
-        }
-        StringBuilder iniciales = new StringBuilder();
-
-        for(String palabra : palabras){
-            if (!palabra.isEmpty()){
-                iniciales.append(palabra.charAt(0)).append(".");
+            if (palabras.length == 1) {
+                inicial= palabras[0].substring(0, 1)+".";
             }
+            else if(palabras.length > 1){
+                for(String palabra : palabras){
+                    if (palabra.matches(".*\\d.")){
+                        throw new IllegalAccessException("Error: El nombre y apellido no pueden contener valores numéricos");
+                    }
+                inicial += palabra.substring(0, 1)+".";
+                }
+            }
+            else {
+                throw new IllegalAccessException("Error: El nombre y apellido no pueden estar vacíos");
+            }
+
+        } catch (IllegalAccessException e) {
+           System.out.println(e.getMessage());
         }
-        return iniciales.toString();
+        return inicial.toUpperCase();
     }
 }

de302df8f1630a755cf4af79b4ab0dffb19d1e23
jkort <jortealv@myuax.com>
2023-03-25 12:46:47 +0100

    PruebaFuncionamiento

---
+++ src/main/java/pruebas/Main.java
@@ -0,0 +1,9 @@
+package pruebas;
+
+public class Main {
+    public static void main(String[] args) {
+        String cadena = "Hola mundo";
+        String iniciales = ClaseCadena.devuelveIniciales(cadena);
+        System.out.println("Las iniciales de: " + cadena + " son: " + iniciales);
+    }
+}

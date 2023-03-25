4c125d8f046127ae008735d1212f90e7f20be5cd
jkort <jortealv@myuax.com>
2023-03-25 12:44:29 +0100

    MetodoPrincipalPrueba

--- src/main/java/pruebas/ClaseCadena.java
+++ src/main/java/pruebas/ClaseCadena.java
@@ -4,5 +4,17 @@ public class ClaseCadena {
 
     public static String devuelveIniciales(String cadena){
         String[] palabras = cadena.split("");
+
+        if(palabras.length < 1){
+            return "";
+        }
+        StringBuilder iniciales = new StringBuilder();
+
+        for(String palabra : palabras){
+            if (!palabra.isEmpty()){
+                iniciales.append(palabra.charAt(0)).append(".");
+            }
+        }
+        return iniciales.toString();
     }
 }

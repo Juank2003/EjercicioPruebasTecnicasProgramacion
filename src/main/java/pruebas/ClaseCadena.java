c11db027ab3e900c20aff6fd85aadbad949788cd
jkort <jortealv@myuax.com>
2023-03-25 13:27:39 +0100

    CorreccionFinal

--- src/main/java/pruebas/ClaseCadena.java
+++ src/main/java/pruebas/ClaseCadena.java
@@ -5,10 +5,17 @@ public class ClaseCadena {
     public static String devuelveIniciales(String cadena){
         String inicial = "";
         try {
+            if (cadena == null || cadena.isEmpty()) {
+                throw new IllegalArgumentException("Error: El nombre y apellido no pueden estar vacíos");
+            }
+
             String[] palabras = cadena.split(" ");
 
-            if (palabras.length == 1) {
-                inicial= palabras[0].charAt(0)+".";
+            if (palabras.length == 1 && palabras[0].matches("^[a-zA-Z]+$")) {
+                throw new IllegalArgumentException("Error: Debe proporcionar al menos un nombre y un apellido separados por un espacio.");
+            }
+            else if (palabras.length == 1 && !palabras[0].matches("^[a-zA-Z]+$")) {
+                throw new IllegalAccessException("Error: El nombre y apellido no pueden contener caracteres especiales o valores numéricos");
             }
             else if(palabras.length > 1){
                 for(String palabra : palabras){
@@ -19,11 +26,11 @@ public class ClaseCadena {
                 }
             }
             else {
-                throw new IllegalAccessException("Error: El nombre y apellido no pueden estar vacíos");
+                throw new IllegalArgumentException("Error: Debe proporcionar al menos un nombre y un apellido separados por un espacio.");
             }
 
-        } catch (IllegalAccessException e) {
-           System.out.println(e.getMessage());
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            System.out.println(e.getMessage());
         }
         return inicial.toUpperCase();
     }
